ec8fd0bbd48ec4e1a3cc8dcd1d7a7890fd72dabb
Alex Ferreira Santos <alexfstos@gmail.com>
2024-01-21 11:40:18 -0300

    feat: add unit tests to category entity

---
+++ src/domain/__tests__/category.entity.test.ts
@@ -0,0 +1,124 @@
+import { Category } from "../category.entity";
+
+describe("Category Unit Tests", () => {
+  describe("constructor", () => {
+    it("should create a Category", () => {
+      let category = new Category({
+        name: "Movie",
+      });
+
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    it("should create a Category with name and description", () => {
+      const category = new Category({
+        name: "Movie",
+        description: "Movie description",
+      });
+
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("Movie description");
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    it("should create a Category with all values", () => {
+      const created_at = new Date();
+      const category = new Category({
+        name: "Movie",
+        description: "Movie description",
+        is_active: false,
+        created_at: created_at,
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("Movie description");
+      expect(category.is_active).toBeFalsy();
+      expect(category.created_at).toBe(created_at);
+    });
+  });
+
+  describe("create command", () => {
+    it("should create a Category", () => {
+      let category = Category.create({
+        name: "Movie",
+      });
+
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    it("should create a Category with description", () => {
+      const category = Category.create({
+        name: "Movie",
+        description: "Movie description",
+      });
+
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("Movie description");
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    it("should create a Category with is_active", () => {
+      const category = Category.create({
+        name: "Movie",
+        is_active: false,
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeFalsy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+  });
+
+  describe("change name", () => {
+    it("should change the Category name", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+      category.changeName("other name");
+      expect(category.name).toBe("other name");
+    });
+  });
+
+  describe("change description", () => {
+    it("should change the Category description", () => {
+      const category = Category.create({
+        name: "Movie",
+        description: "Movie description",
+      });
+      category.changeDescription("other description");
+      expect(category.description).toBe("other description");
+    });
+  });
+
+  describe("change is_active", () => {
+    it("should active a Category", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+      category.activate();
+      expect(category.is_active).toBe(true);
+    });
+
+    it("should deactive a Category", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+      category.deactivate();
+      expect(category.is_active).toBe(false);
+    });
+  });
+
+});
