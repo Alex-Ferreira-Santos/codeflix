5cbc48e7b2dba0352e1540042816fe63f17048eb
Alex Ferreira Santos <alexfstos@gmail.com>
2024-02-04 14:34:42 -0300

    feat: add categories providers and inject dependencies into the repository

--- src/nest-modules/categories-module/categories.controller.test.ts
+++ src/nest-modules/categories-module/categories.controller.test.ts
@@ -1,19 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
-import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
-import { SequelizeModule, getModelToken } from '@nestjs/sequelize';
 import { DatabaseModule } from 'src/nest-modules/database-module/database.module';
 import { CategoriesModule } from './categories.module';
 import { ConfigModule } from 'src/nest-modules/config-module/config.module';
-import { ConfigService } from '@nestjs/config';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
+      imports: [ConfigModule.forRoot({}), DatabaseModule, CategoriesModule],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { CreateCategoryUseCase } from '@core/category/application/usecases/create-category/create-category.use-case';
+import { UpdateCategoryUseCase } from '@core/category/application/usecases/update-category/update-category.use-case';
+import { DeleteCategoryUseCase } from '@core/category/application/usecases/delete-category/delete-category.use-case';
+import { ListCategoriesUseCase } from '@core/category/application/usecases/list-categories/list-categories.use-case';
+import { GetCategoryUseCase } from '@core/category/application/usecases/get-category/get-category.use-case';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoryRepo: CategorySequelizeRepository) {
-  }
+
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteeUseCase: DeleteCategoryUseCase
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase
+
+  @Inject(ListCategoriesUseCase)
+  private listUseCase: ListCategoriesUseCase
+
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
